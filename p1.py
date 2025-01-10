c9bc6316ee6c30c5aaef6f85962e66bd3f2ee190
samruddhimohite-15 <mohitesamruddhi58@gmail.com>
2025-01-10 01:46:25 -0800

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,23 @@
+a=(10,20,30,40)
+print(list(a))
+print(set(a))
+print(str(a))
+
+
+
+
+num1=input("enter num1 value :")
+num2=input("enter num2 value :")
+new_num1=int(num1)
+new_num2=int(num2)
+add=new_num1+new_num2
+print("The addition of two given number is:",add)
+
+
+
+num1=input("enter num1 value :")
+num2=input("enter num2 value :")
+add=int(num1)+int(num2)
+print ("the addition of two given numbers is:",add)
+
+
